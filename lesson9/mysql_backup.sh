f33319e7197d9e39c4255f607292ddd66fbf17d7
Anna <anxlime@gmail.com>
2026-09-15 18:57:03 +0300

    delete old files

--- lesson9/mysql_backup.sh
+++
@@ -1,7 +0,0 @@
-#/bin/bash
-set -e
-
-current_date=$(date +%s)
-mysqldump "lesson9" > "/opt/mysql_backup/lesson9_${current_date}.sql"
-
-rsync -av "/opt/mysql_backup/lesson9_${current_date}.sql" 10.0.3.4::mysql/
